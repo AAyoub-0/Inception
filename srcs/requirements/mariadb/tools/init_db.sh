bf89daabda9162d2af78ccdf9568414fd2ba6864
aboumall <aboumall42@gmail.com>
2026-02-13 03:53:38 +0100

    fix(wip): trying to fix wordpress connection to mariadb

---
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+DIR_DATA=/var/lib/mysql
+USER=mysql
+
+DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
+DB_USER_PASSWORD=$(cat /run/secrets/db_password)
+
+# Initialisation si nécessaire
+if [ ! -d "$DIR_DATA/mysql" ]; then
+    echo "Initializing MariaDB data directory..."
+    mariadb-install-db --user=$USER --datadir=$DIR_DATA
+
+    echo "Creating database and user via bootstrap..."
+    mysqld --user=$USER --datadir=$DIR_DATA --bootstrap <<SQL
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${DB_USER_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+FLUSH PRIVILEGES;
+SQL
+fi
+
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld
+
+# Lancer MariaDB en foreground
+exec mysqld --user=$USER --datadir=$DIR_DATA
--- srcs/requirements/wordpress/tools/install_wordpress.sh
+++ srcs/requirements/wordpress/tools/install_wordpress.sh
@@ -1,21 +1,48 @@
 #!/bin/sh
 
-cd /var/www/html
+set -e
 
-echo "Installing WordPress..."
+WP_PATH=/var/www/html
 
-if [ ! -f index.php ]; then
-    echo "Downloading WordPress..."
-    curl -O https://wordpress.org/latest.tar.gz
-    echo "Extracting WordPress..."
-    tar -xzf latest.tar.gz --strip-components=1
-    echo "WordPress installed successfully."
-    rm latest.tar.gz
+echo "Waiting for MariaDB..."
+sleep 10
+
+echo "MariaDB is up!"
+
+if [ ! -f "$WP_PATH/wp-config.php" ]; then
+  echo "Installing WordPress..."
+
+  wp core download --path="$WP_PATH" --allow-root
+
+  echo "WordPress downloaded."
+
+  echo "Configuring WordPress..."
+
+  wp config create \
+    --path="$WP_PATH" \
+    --dbname="$WORDPRESS_DB_NAME" \
+    --dbuser="$WORDPRESS_DB_USER" \
+    --dbpass="$(cat $WORDPRESS_DB_PASSWORD_FILE)" \
+    --dbhost="$WORDPRESS_DB_HOST" \
+    --allow-root
+
+  wp core install \
+    --path="$WP_PATH" \
+    --url="https://$DOMAIN_NAME" \
+    --title="Inception" \
+    --admin_user="$WP_ADMIN_USER" \
+    --admin_password="$(cat $WP_ADMIN_PASSWORD_FILE)" \
+    --admin_email="$WP_ADMIN_EMAIL" \
+    --allow-root
+
+  wp user create \
+    "$WP_USER_USER" \
+    "$WP_USER_EMAIL" \
+    --role=author \
+    --user_pass="$(cat $WP_USER_PASSWORD_FILE)" \
+    --path="$WP_PATH" \
+    --allow-root
 fi
 
-if ! id www-data >/dev/null 2>&1; then
-    addgroup -g 82 www-data
-    adduser -D -u 82 -G www-data -s /sbin/nologin www-data
-fi
-
-chown -R www-data:www-data /var/www/html
+echo "Starting PHP-FPM..."
+exec php-fpm81 -F
