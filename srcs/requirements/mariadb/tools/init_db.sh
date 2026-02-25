6465e3651f1889c039044c6e6b6ac830320bce4b
AAyoub-El-Boss <yousalama5@gmail.com>
2026-02-26 01:53:49 +0200

    fix: fixed mariadb script

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 set -e
 
 DIR_DATA=/var/lib/mysql
@@ -12,15 +11,25 @@ DB_USER_PASSWORD=$(cat /run/secrets/db_password)
 if [ ! -d "$DIR_DATA/mysql" ]; then
     echo "Initializing MariaDB data directory..."
     mariadb-install-db --user=$USER --datadir=$DIR_DATA
-fi
 
-echo "Creating database and user via bootstrap..."
-    mysqld --user=$USER --datadir=$DIR_DATA --bootstrap <<SQL
-    CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-    CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${DB_USER_PASSWORD}';
-    GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
-    FLUSH PRIVILEGES;
-SQL
+    echo "Starting temporary MariaDB server..."
+    mysqld --user=$USER --datadir=$DIR_DATA --skip-networking &
+    pid="$!"
+
+    # Attendre que le serveur démarre
+    until mysqladmin ping --silent; do
+        sleep 1
+    done
+
+    echo "Creating database and user..."
+    mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+    mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${DB_USER_PASSWORD}';"
+    mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
+    mysql -e "FLUSH PRIVILEGES;"
+
+    # Stopper le serveur temporaire
+    kill "$pid"
+fi
 
 mkdir -p /run/mysqld
 chown -R mysql:mysql /run/mysqld
